ae1410d4b2d719633144569fe0ec26afb6026517
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-04-24 19:51:17 -0300

    feat(db): adiciona parametros de configuracao por variaveis de ambiente

--- db/config/connection.php
+++ db/config/connection.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+// $servername = getenv("DB_SERVER_NAME");
+// $username = getenv("DB_USER");
+// $password = getenv("DB_PASSWORD");
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli(getenv("DB_HOSTNAME"), getenv("DB_USER"), getenv("DB_PASSWORD"));
 
 // Check connection
 if ($conn->connect_error) {
