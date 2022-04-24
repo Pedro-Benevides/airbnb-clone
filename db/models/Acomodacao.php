b2e6b9479abbc7e149d2eedab4b8dc24be3ddcaf
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-04-24 19:35:55 -0300

    chore(models/Acomodacao): remove atribuicao duplicada de estado

--- db/models/Acomodacao.php
+++ db/models/Acomodacao.php
@@ -25,7 +25,6 @@ class Acomodacao
         $this->cidade = $cidade;
         $this->estado = $estado;
         $this->pais = $pais;
-        $this->estado = $estado;
         $this->complemento = $complemento;
         $this->diaria = $diaria;
     }
