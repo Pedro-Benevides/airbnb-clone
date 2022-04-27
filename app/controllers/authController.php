3f74793df4c5de463f24974d62261ebf1334fc3d
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-04-27 18:58:50 -0300

    refact(authController): ajusta para uso da classe de querys, adicinoa atributo de usuario logado; implementa método de logout

--- app/controllers/authController.php
+++ app/controllers/authController.php
@@ -1,35 +1,76 @@
 <?php
 
+use app\models\BaseQuery;
 use app\models\Usuario;
 
 class AuthController
 {
 
     private $usuarioRepo;
+    private $db;
+    private $user;
 
-    public function __construct(Usuario $usuarioRepo)
+    public function __construct(mysqli $db)
     {
-        $this->usuarioRepo = $usuarioRepo;
+        $this->db = $db;
+        $this->usuarioRepo = new BaseQuery('usuario');
     }
 
-    public function login(array $requestData)
+    public function getUser(): Usuario
+    {
+        return $this->user;
+    }
+
+    public function setUser(Usuario $user = null)
+    {
+        $this->user = $user;
+    }
+
+    public function login(array $requestData): ?Usuario
     {
         $canLogin = $this->validate($requestData);
 
         if ($canLogin) {
-            return $this->usuarioRepo->whereQuery(
-                'email',
-                '=',
-                $requestData['email']
-            ) . $this->usuarioRepo->whereQuery(
-                'senha',
-                '=',
-                password_hash(trim($requestData['senha']), PASSWORD_BCRYPT),
-                'AND'
-            );
+            $dbUser = $this->db->query(
+
+                $this->usuarioRepo->whereQuery(
+                    'email',
+                    '=',
+                    $requestData['email']
+                ) . $this->usuarioRepo->whereQuery(
+                    'senha',
+                    '=',
+                    password_hash(htmlspecialchars(stripslashes(trim($requestData['senha']))), PASSWORD_BCRYPT),
+                    'AND'
+                )
+            )->fetch_all(MYSQLI_ASSOC);
+
+            $dbUser = array_map(function ($row) {
+                return new Usuario(
+                    $row['nome'],
+                    $row['cpf'],
+                    $row['email'],
+                    $row['senha'],
+                    $row['pais'],
+                    $row['telefone'],
+                    $row['cartaoId'], //TODO: puxar relacionamento com cartao
+                    $row['anfitriao'],
+                    $row['locatario']
+                );
+                printf("%s (%s)\n", $row["Name"], $row["CountryCode"]);
+            }, $dbUser)->array_shift;
+
+            $this->setUser($dbUser);
+
+            return $dbUser;
         } else {
-            return '422';
+            return null;
         }
+    }
+
+    public function logout()
+    {
+        $this->setUser(null);
     }
 
     private function validate($requestData)
