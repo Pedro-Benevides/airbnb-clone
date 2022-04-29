4de96fc29b98a7078cd13947054ffc7008add886
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-04-28 22:49:26 -0300

    feat(views): implementa pagina de login

---
+++ app/Views/pages/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<!-- UNEB - Universidade do Estado da Bahia -->
+<!-- Alunos- Rafael R C da Cruz, Pedro Victor-->
+<!-- Sistema de Informação. LP3 -->
+
+<head>
+    <link rel="shortcut icon" href="../assets/bglogin.png">
+    <title>Login</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    <!--Stylesheet-->
+    <link rel="stylesheet" type="text/css" href="../styles/login.css">
+
+</head>
+
+<body>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form action="" method="POST">
+        <h3>Login Here</h3>
+
+        <label for="email">Email></label>
+        <input type="email" placeholder="Email" id="email">
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password">
+
+        <button>Log In</button>
+        <!-- <div class="social">
+            <div class="go"><i class="fab fa-google"></i> Google</div>
+            <div class="fb"><i class="fab fa-facebook"></i> Facebook</div>
+        </div> -->
+    </form>
+</body>
+
+</html>
