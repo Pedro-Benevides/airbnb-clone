13b60952d0365b341717cfdcc4e978748b149bd0
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-05-02 18:52:07 -0300

    feat(Models): implementa classe para table de locacao

---
+++ app/models/Locacao.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace app\Models;
+
+class Acomodacao
+{
+    //Table Atributes
+    private $usuarioAnfitriaoId;
+    private $usuarioLocatarioId;
+    private $acomodacaoId;
+    private $diaria;
+    private $multa;
+    private $dataInicio;
+    private $dataFim;
+    private $checkin;
+    private $cancelamento;
+
+    function __construct(
+        int $usuarioAnfitriaoId,
+        int $usuarioLocatarioId,
+        int $acomodacaoId,
+        float $diaria,
+        string $dataInicio,
+        string $dataFim,
+        float $multa = null,
+        bool $checkin = false,
+        bool $cancelamento = false
+    ) {
+        $this->usuarioAnfitriaoId = $usuarioAnfitriaoId;
+        $this->usuarioLocatarioId = $usuarioLocatarioId;
+        $this->acomodacaoId = $acomodacaoId;
+        $this->diaria = $diaria;
+        $this->multa = $multa;
+        $this->dataInicio = $dataInicio;
+        $this->dataFim = $dataFim;
+        $this->checkin = $checkin;
+        $this->cancelamento = $cancelamento;
+    }
+
+    public function getAcomodacao()
+    {
+        return $this->acomodacaoId;
+    }
+
+    public function getLocatario()
+    {
+        return $this->usuarioLocatarioId;
+    }
+
+    public function getAnfitriao()
+    {
+        return $this->usuarioAnfitriaoId;
+    }
+
+    public function getDataFim()
+    {
+        return $this->dataFim;
+    }
+
+    public function getDataInicio()
+    {
+        return $this->dataInicio;
+    }
+
+    public function getMulta()
+    {
+        return $this->multa;
+    }
+
+    public function getDiaria()
+    {
+        return $this->diaria;
+    }
+
+    public function getCheckin()
+    {
+        return $this->checkin;
+    }
+
+    public function getCancelamento()
+    {
+        return $this->cancelamento;
+    }
+}
