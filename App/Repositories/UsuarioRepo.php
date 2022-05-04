5b7e82b386c9405da1527edca912b2113482c924
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-05-03 23:32:05 -0300

    feat(Repositories): implementa repo de usuario com métodos create e auth

---
+++ App/Repositories/UsuarioRepo.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Repositories;
+
+require_once dirname(dirname(__FILE__)) . '\Models\Usuario.php';
+require_once 'BaseRepository.php';
+
+use App\Models\Usuario;
+use App\Repositories\BaseRepository;
+use mysqli;
+
+class UsuarioRepo extends BaseRepository
+{
+    private $db;
+
+    public function __construct(mysqli $db)
+    {
+        $this->db = $db;
+        parent::__construct('usuario');
+    }
+
+    public function create(array $userForm)
+    {
+        $columns = array_keys($userForm);
+        $values = array_values($userForm);
+
+        return
+            $this->db->query(
+                $this->insertQuery($columns, $values)
+            );
+    }
+
+    public function auth(array $credentials): ?Usuario
+    {
+        $result = $this->db->query(
+            $this->searchQuery(
+                ' email ',
+                ' = ',
+                $credentials['email']
+            ) . $this->whereQuery(
+                ' senha ',
+                ' = ',
+                md5(
+                    htmlspecialchars(
+                        stripslashes(
+                            trim(
+                                $credentials['password']
+                            )
+                        )
+                    )
+                ),
+                ' and '
+            )
+        )->fetch_all(MYSQLI_ASSOC);
+
+        $userArray = $this->buildUsuario($result);
+
+        return array_shift($userArray);
+    }
+
+    private function buildUsuario($queryResult)
+    {
+        return array_map(function ($row) {
+            return new Usuario(
+                $row['nome'],
+                $row['cpf'],
+                $row['email'],
+                $row['senha'],
+                $row['pais'],
+                $row['telefone'],
+                $row['anfitriao'],
+                $row['locatario'],
+                $row['cartao_id'], //TODO: puxar relacionamento com cartao
+            );
+        }, $queryResult);
+    }
+}
