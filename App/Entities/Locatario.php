190f1bdee04886bb1782b16d05b79b8176ccbe37
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-05-27 00:04:14 -0300

    feat(Locatario): ajusta tipagem de Cartao; adiciona atributos faltantes ao construtor; adiciona descrição da classe

--- App/Entities/Locatario.php
+++ App/Entities/Locatario.php
@@ -3,9 +3,20 @@
 namespace App\Entities;
 
 use App\Models\Usuario;
+use App\Models\Cartao;
 
 require_once dirname(dirname(__FILE__)) . '\Models\Usuario.php';
+require_once dirname(dirname(__FILE__)) . '\Models\Cartao.php';
 
+/**
+ * Class Locatario
+ *
+ * @property Cartao $cartao
+ * @property array $locacoes
+ * @property array $comentarios
+ *
+ * @package App\Entities
+ */
 class Locatario extends Usuario
 {
     private $cartao;
@@ -13,8 +24,15 @@ class Locatario extends Usuario
     private $comentarios;
 
     public function __construct(
-        Usuario $usuario
+        Usuario $usuario,
+        Cartao $cartao,
+        array $locacoes = array(),
+        array $comentarios = array()
     ) {
+        $this->cartao = $cartao;
+        $this->locacoes = $locacoes;
+        $this->comentarios = $comentarios;
+
         parent::__construct(
             $usuario->nome,
             $usuario->cpf,
@@ -27,8 +45,8 @@ class Locatario extends Usuario
 
     /**
      * Get the value of cartao
-     */ 
-    public function getCartao()
+     */
+    public function getCartao(): Cartao
     {
         return $this->cartao;
     }
@@ -37,8 +55,8 @@ class Locatario extends Usuario
      * Set the value of cartao
      *
      * @return  self
-     */ 
-    public function setCartao($cartao)
+     */
+    public function setCartao(Cartao $cartao)
     {
         $this->cartao = $cartao;
 
@@ -47,7 +65,7 @@ class Locatario extends Usuario
 
     /**
      * Get the value of locacoes
-     */ 
+     */
     public function getLocacoes()
     {
         return $this->locacoes;
@@ -57,7 +75,7 @@ class Locatario extends Usuario
      * Set the value of locacoes
      *
      * @return  self
-     */ 
+     */
     public function setLocacoes($locacoes)
     {
         $this->locacoes = $locacoes;
@@ -67,7 +85,7 @@ class Locatario extends Usuario
 
     /**
      * Get the value of comentarios
-     */ 
+     */
     public function getComentarios()
     {
         return $this->comentarios;
@@ -77,7 +95,7 @@ class Locatario extends Usuario
      * Set the value of comentarios
      *
      * @return  self
-     */ 
+     */
     public function setComentarios($comentarios)
     {
         $this->comentarios = $comentarios;
