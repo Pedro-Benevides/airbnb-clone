0a31044b55a22562c41384dee9e6f0c57bc7f40c
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-05-27 00:03:29 -0300

    feat(Anfitriao): adiciona descrição das propriedades da classe; adiciona atributos faltantes no construtor

--- App/Entities/Anfitriao.php
+++ App/Entities/Anfitriao.php
@@ -6,14 +6,27 @@ use App\Models\Usuario;
 
 require_once dirname(dirname(__FILE__)) . '\Models\Usuario.php';
 
+/**
+ * Class Locatario
+ *
+ * @property array $locacoes
+ * @property array $acomodacoes
+ *
+ * @package App\Entities
+ */
 class Anfitriao extends Usuario
 {
     private $locacoes;
     private $acomodacoes;
 
     public function __construct(
-        Usuario $usuario
+        Usuario $usuario,
+        array $locacoes = array(),
+        array $acomodacoes = array()
     ) {
+        $this->locacoes = $locacoes;
+        $this->acomodacoes = $acomodacoes;
+
         parent::__construct(
             $usuario->nome,
             $usuario->cpf,
