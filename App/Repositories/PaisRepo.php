f72c3b4e3e7442c33fa884e4716f8620ae4cab68
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-06-16 16:34:44 -0300

    feat(Paisrepo): implementa Repo se Pais necessario para build de usuario

---
+++ App/Repositories/PaisRepo.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+require_once dirname(dirname(__FILE__)) . '\Models\Pais.php';
+require_once dirname(dirname(dirname(__FILE__))) . '\database\config\connection.php';
+require_once 'BaseRepository.php';
+
+use App\Models\Pais;
+use App\Repositories\BaseRepository;
+use Connection;
+use PDO;
+
+class PaisRepo extends BaseRepository
+{
+    private $db;
+
+    public function __construct()
+    {
+        parent::__construct('pais');
+    }
+
+    public function whereId(int $id): ?Pais
+    {
+        $db = Connection::Connect();
+        $result = $db->query(
+            $this->search(
+                ' id ',
+                ' = ',
+                $id
+            )
+        )->fetch(PDO::FETCH_ASSOC);
+
+        return $this->buildPais($result);
+    }
+
+    private function buildPais($queryResult)
+    {
+        return new Pais(
+            $queryResult['name']
+        );
+    }
+}
