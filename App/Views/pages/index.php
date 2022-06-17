0e8ab81c12574aa815038230fb49533ada8086c0
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-06-16 23:20:34 -0300

    refact(index.php): ajusta para funcionar no novo esquema de rotas

--- App/Views/pages/index.php
+++ App/Views/pages/index.php
@@ -1,15 +1,6 @@
 <?php
-require_once dirname(dirname(dirname(dirname(__FILE__)))) . '\database\config\Connection.php';
-require_once dirname(dirname(dirname(__FILE__))) . '\Controllers\AuthController.php';
-
-$auth = new AuthController();
-
-//Autenticação do usuario na sessao
-$auth->login($_POST);
-$_SESSION['AUTH'] = $auth;
-
-if ($_SESSION['AUTH']->getUser()) {
-    echo 'Bem vindo ao site do Airbnb ' . ($_SESSION['AUTH']->getUser()->getNome());
+if (!empty($user)) {
+    echo 'Bem vindo ao site do Airbnb ' . $user->getNome();
 } else {
     echo "Não encontrado";
 }
