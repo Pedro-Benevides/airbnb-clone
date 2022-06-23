6ba5297b7f2fbc45309fda78d22f28335270c0ad
Pedro Benevides <pvsbenevides197@gmail.com>
2022-06-23 14:36:12 -0300

    chore(login): ajusta require

--- App/Views/pages/login.php
+++ App/Views/pages/login.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
     <!--Stylesheet-->
     <style type="text/css">
-        <?php require_once dirname(dirname(__FILE__)) . '\styles\login.php'; ?>
+        <?php require dirname(dirname(__FILE__)) . '\styles\login.php'; ?>
     </style>
 
 </head>
