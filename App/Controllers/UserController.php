c67a662079096e20aa2415f1a0c6f6de710948b9
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-06-26 22:01:31 -0300

    feat(UserController): adiciona action para carregamento do form de registro

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -1,23 +1,33 @@
 <?php
 // require_once '../Models/Usuario.php';
 require_once dirname(dirname(__FILE__)) . '\Repositories\UsuarioRepo.php';
+require_once dirname(dirname(__FILE__)) . '\Repositories\PaisRepo.php';
 
 use App\Models\Usuario;
+use App\Repositories\PaisRepo;
 use App\Repositories\UsuarioRepo;
 
 class UserController
 {
 
     private $usuarioRepo;
+    private $paisRepo;
 
     public function __construct()
     {
         $this->usuarioRepo = new UsuarioRepo();
+        $this->paisRepo = new PaisRepo();
     }
 
-    public function create(array $userForm)
+    public function getForm()
     {
-        $user = $this->usuarioRepo->create($userForm);
+        $paises = $this->paisRepo->getAll();
+        require dirname(dirname(__FILE__)) . '\Views\pages\register.php';
+    }
+
+    public function create()
+    {
+        $user = $this->usuarioRepo->create($_POST);
 
         if ($user) {
             return "<br> Usuario cadastrado com sucesso!";
