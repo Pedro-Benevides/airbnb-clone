7de092f5590ac1d0173b728af53a54eebfe1842b
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-07-03 17:32:19 -0300

    chore(userController): ajusta uso de repos

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -1,33 +1,31 @@
 <?php
-// require_once '../Models/Usuario.php';
 require_once dirname(dirname(__FILE__)) . '\Repositories\UsuarioRepo.php';
 require_once dirname(dirname(__FILE__)) . '\Repositories\PaisRepo.php';
 
-use App\Models\Usuario;
 use App\Repositories\PaisRepo;
 use App\Repositories\UsuarioRepo;
 
 class UserController
 {
 
-    private $usuarioRepo;
-    private $paisRepo;
-
     public function __construct()
     {
-        $this->usuarioRepo = new UsuarioRepo();
-        $this->paisRepo = new PaisRepo();
     }
 
-    public function getForm()
+    public function form()
     {
-        $paises = $this->paisRepo->all();
-        require dirname(dirname(__FILE__)) . '\Views\pages\register.php';
+        $paisRepo = new PaisRepo();
+
+        $paises = $paisRepo->all();
+
+        require dirname(dirname(__FILE__)) . '\Views\pages\userRegister.php';
     }
 
     public function create()
     {
-        $user = $this->usuarioRepo->create($_POST);
+        $usuarioRepo = new UsuarioRepo();
+
+        $user = $usuarioRepo->create($_POST);
 
         if ($user) {
             require dirname(dirname(__FILE__)) . '\Views\pages\login.php';
@@ -38,7 +36,9 @@ class UserController
 
     public function getUser()
     {
-        $userAuth = $this->usuarioRepo->whereId($_SESSION['AUTH']);
+        $usuarioRepo = new UsuarioRepo();
+
+        $userAuth = $usuarioRepo->whereId($_SESSION['AUTH']);
 
         require dirname(dirname(__FILE__)) . '\Views\pages\index.php';
     }
