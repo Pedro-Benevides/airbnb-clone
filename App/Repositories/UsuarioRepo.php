ad7b5feeb14b0c17a4f574c588c98395a52d9f0b
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-07-03 22:19:26 -0300

    feat(UsuarioRepo): implementa build para usuario Anfitrião e método para usuario completo

--- App/Repositories/UsuarioRepo.php
+++ App/Repositories/UsuarioRepo.php
@@ -4,9 +4,12 @@ namespace App\Repositories;
 
 require_once dirname(dirname(dirname(__FILE__))) . '\database\config\connection.php';
 require_once dirname(dirname(__FILE__)) . '\Models\Usuario.php';
-require_once dirname(dirname(__FILE__)) . '\Repositories\PaisRepo.php';
+require_once dirname(dirname(__FILE__)) . '\Entities\Anfitriao.php';
+require_once 'PaisRepo.php';
 require_once 'BaseRepository.php';
+require_once 'LocacaoRepo.php';
 
+use App\Entities\Anfitriao;
 use App\Entities\Locatario;
 use App\Models\Usuario;
 use App\Repositories\BaseRepository;
@@ -78,9 +81,10 @@ class UsuarioRepo extends BaseRepository
      * @param int option valor para configurar o retorno, default: 0 traz um usuario padrão, 1 retorna um Locatario, 2 retorna um Anfitriao
      * 
      */
-    public function whereId(int $id, $option = 0)
+    public function whereId(int $id, $option = 0, $includeDetails = false)
     {
         $usuario = null;
+
         $db = Connection::Connect();
         $result = $db->query(
             $this->search(
@@ -95,7 +99,7 @@ class UsuarioRepo extends BaseRepository
                 $usuario = $this->buildLocatario($result);
                 break;
             case 2:
-                // $usuario = $this->buildAnfitriao($result);
+                $usuario = $this->buildAnfitriao($result);
                 break;
             default:
                 $usuario = $this->buildUsuario($result);
@@ -105,7 +109,47 @@ class UsuarioRepo extends BaseRepository
         return $usuario;
     }
 
-    private function buildUsuario($queryResult)
+    public function getComplete(int $id): ?array
+    {
+        $locacaoRepo = new LocacaoRepo();
+
+        $db = Connection::Connect();
+        $result = $db->query(
+            $this->search(
+                ' id ',
+                ' = ',
+                $id
+            )
+        )->fetch(PDO::FETCH_ASSOC);
+
+        $stateAnfitriao = $this->buildAnfitriao($result);
+        $stateAnfitriao->setLocacoes(array_map(function ($acomodacao) use ($locacaoRepo) {
+
+            return $locacaoRepo->whereAcomodacaoId($acomodacao->getId());
+        }, $stateAnfitriao->getAcomodacoes()));
+
+        $stateLocatario = $this->buildLocatario($result);
+        $stateLocatario->setLocacoes($locacaoRepo->whereLocatarioId($stateLocatario->getId()));
+
+        $anfitrioesLocacoes = array_map(function ($locacaoLocatario) {
+
+            return $locacaoLocatario->getAcomodacao()->getProprietario();
+        }, $stateLocatario->getLocacoes());
+
+        return [
+            'nome' => $stateLocatario->getNome(),
+            'cpf' => $stateLocatario->getCpf(),
+            'email' => $stateLocatario->getEmail(),
+            'telefone' => $stateLocatario->getTelefone(),
+            'cartao' => $stateLocatario->getCartao(),
+            'acomodacoes' => $stateAnfitriao->getAcomodacoes(),
+            'locacoesAnfitriao' => $stateAnfitriao->getLocacoes(),
+            'locacoesLocatario' => $stateLocatario->getLocacoes(),
+            'anfitrioesLocacoes' => $anfitrioesLocacoes
+        ];
+    }
+
+    private function buildUsuario($queryResult): Usuario
     {
         $paisRepo = new PaisRepo();
         $userPais = $paisRepo->whereId($queryResult['pais_id']);
@@ -128,7 +172,7 @@ class UsuarioRepo extends BaseRepository
         return $user;
     }
 
-    private function buildLocatario($queryResult)
+    private function buildLocatario($queryResult): Locatario
     {
         $paisRepo = new PaisRepo();
         $userPais = $paisRepo->whereId($queryResult['pais_id']);
@@ -146,14 +190,40 @@ class UsuarioRepo extends BaseRepository
             $queryResult['locatario'],
         );
 
-        $user->setId($queryResult['id']);
-
         $cartaoRepo = new CartaoRepo();
         $cartao = $cartaoRepo->whereUsuarioId($queryResult['id']);
 
         $locatario = new Locatario($user, $cartao);
+        $locatario->setId($queryResult['id']);
 
         return $locatario;
+    }
+
+    private function buildAnfitriao($queryResult): Anfitriao
+    {
+        $paisRepo = new PaisRepo();
+        $userPais = $paisRepo->whereId($queryResult['pais_id']);
+
+        $queryResult['pais'] = $userPais;
+
+        $user = new Usuario(
+            $queryResult['nome'],
+            $queryResult['cpf'],
+            $queryResult['email'],
+            $queryResult['senha'],
+            $queryResult['pais'],
+            $queryResult['telefone'],
+            $queryResult['anfitriao'],
+            $queryResult['locatario'],
+        );
+
+        $acomodacaoRepo = new AcomodacaoRepo();
+        $acomodacoes = $acomodacaoRepo->whereUsuarioId($queryResult['id']);
+
+        $anfitriao = new Anfitriao($user, $acomodacoes);
+        $anfitriao->setId($queryResult['id']);
+
+        return $anfitriao;
     }
 
     private function encrypt($password)
